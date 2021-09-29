452a9ddc6e243bba41877d22312e3702b7c04d01
Zaier Vera <veraalber989@gmail.com>
2021-09-28 21:53:17 -0300

    Agregando codigo para interactuar con el programa en el navegador

--- figuras_geometricas/triangulo/triangulo.js
+++ figuras_geometricas/triangulo/triangulo.js
@@ -10,3 +10,26 @@ function areaTriangulo(base, altura) {
 }
 
 console.groupEnd();
+
+// Aqui interatuamos con el HTML
+function calcularPerimetroTriangulo() {
+    const input1 = document.getElementById("InputTriangulo1");
+    const value1 = Number(input1.value);
+    const input2 = document.getElementById("InputTriangulo2");
+    const value2 = Number(input2.value);
+    const base = document.getElementById("InputBaseTriangulo");
+    const value3 = Number(base.value);
+    const perimetro = perimetroTriangulo(value1, value2, value3);
+    alert("El perimetro del triangulo es: " + perimetro);
+}
+
+function calcularAreaTriangulo() {
+    const input1 = document.getElementById("InputTriangulo1");
+    const value1 = Number(input1.value);
+    const input2 = document.getElementById("InputTriangulo2");
+    const value2 = Number(input2.value);
+    const base = document.getElementById("InputBaseTriangulo");
+    const value3 = Number(base.value);
+    const area = areaTriangulo(value1, value2, value3);
+    alert("El perimetro del triangulo es: " + area);
+}
